b8917ded567b6aa6732d261307750bc73d53b402
Nathan <nathannxx@proton.me>
2026-02-07 13:04:10 +0100

    tanzanite: sync with device tree
    Signed-off-by: Nathan <nathannxx@proton.me>

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),tanzanite)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,b3330507508fcbfa1bcf9e15e7f5072e1ce15825)
+$(call add-radio-file-sha1-checked,radio/gz.img,8fbd78022f6f13c59b826d1436d3e39cb2edabb9)
+$(call add-radio-file-sha1-checked,radio/lk.img,697c4fd47bdd1800ea1ff08f432c13bff767fd53)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,c3c5fba96ab3472be7d955367f91a58ae6875c88)
+$(call add-radio-file-sha1-checked,radio/md1img.img,def1ff251e6da0a408b7938c8916f95f63c7c6dc)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,72deec53e7e8992d77fba545a9bbcc1706958d6b)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,ad68beab35a8320bb56040410b3639d38f3d2eb5)
+$(call add-radio-file-sha1-checked,radio/scp.img,86edbcecb7f474a629e0aa519dcd310cd9d598f0)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,b565acaef999ab3be6383d82dc552ae8c32577c8)
+$(call add-radio-file-sha1-checked,radio/sspm.img,ea9b410cf68212a79aceab99292f5a59753b8a65)
+$(call add-radio-file-sha1-checked,radio/tee.img,9bf89b62962163687ff7368ecb77e149fa967646)
+
+endif
